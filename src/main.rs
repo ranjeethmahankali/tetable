8c9b0e846447940c6cd5db5225cc35382415df9e
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-10-26 10:52:51 -0400

    Get the basic demo of a unit cube going

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+mod util;
+
+use three_d::{
+    AmbientLight, Camera, ClearState, DirectionalLight, FrameOutput, InnerSpace, Srgba, Window,
+    WindowSettings, degrees, vec3,
+};
+use util::{CameraMouseControl, PolygonMesh, mesh_view, wireframe_view};
+
+fn main() {
+    // Window and context.
+    let window = Window::new(WindowSettings {
+        title: "Example: Rendering a Triangle Mesh".to_string(),
+        min_size: (512, 256),
+        ..Default::default()
+    })
+    .unwrap();
+    let context = window.gl();
+    // Setup the camera and the controls and lights.
+    let target = vec3(0.5, 0.5, 0.5);
+    let scene_radius: f32 = 6.0;
+    let mut camera = Camera::new_perspective(
+        window.viewport(),
+        target + scene_radius * vec3(0.6, 0.3, 1.0).normalize(),
+        target,
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        1000.0,
+    );
+    let mut control =
+        CameraMouseControl::new(*camera.target(), 0.1 * scene_radius, 100.0 * scene_radius);
+    let ambient = AmbientLight::new(&context, 0.7, Srgba::WHITE);
+    let directional0 = DirectionalLight::new(&context, 2.0, Srgba::WHITE, &vec3(-1.0, -1.0, -1.0));
+    let directional1 = DirectionalLight::new(&context, 2.0, Srgba::WHITE, &vec3(1.0, 1.0, 1.0));
+    // Create the mesh.
+    let mut mesh = PolygonMesh::unit_box().unwrap();
+    mesh.update_vertex_normals_accurate().unwrap();
+    let view = mesh_view(&mesh, &context);
+    let (vertices, edges) = wireframe_view(&mesh, &context, 0.01, 0.005);
+    // Render loop.
+    window.render_loop(move |mut frame_input| {
+        let mut redraw = frame_input.first_frame;
+        redraw |= camera.set_viewport(frame_input.viewport);
+        redraw |= control.handle_events(&mut camera, &mut frame_input.events);
+        if redraw {
+            frame_input
+                .screen()
+                .clear(ClearState::color_and_depth(0.1, 0.1, 0.1, 1.0, 1.0))
+                .render(
+                    &camera,
+                    view.into_iter().chain(&vertices).chain(&edges),
+                    &[&ambient, &directional0, &directional1],
+                );
+        }
+        FrameOutput {
+            swap_buffers: redraw,
+            ..Default::default()
+        }
+    });
+}
